9c921152835df3ee61a517377bca689f3b38b609
Riley Muessig <rjm7793@rit.edu>
2020-07-05 22:01:24 -0400

    Implemented BackgroundImages into the GUI and set up the GUI stage so that it is now viewable. Resized Nodes.

--- src/gui/ChessGUI.java
+++ src/gui/ChessGUI.java
@@ -4,12 +4,16 @@ import chess_game.ChessGame;
 import chess_game.Square;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import pieces.ChessPiece;
 
 public class ChessGUI extends Application {
 
@@ -63,26 +67,68 @@ public class ChessGUI extends Application {
     public void start(Stage stage) {
         buttons = new Button[8][8];
         gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        populateGridPane();
 
         vBox = new VBox();
-        for (int i = 1; i < 9; i++) {
-            Label rowName = new Label(String.valueOf(i));
-            vBox.getChildren().add(rowName);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setTranslateX(630);
+        for (int i = 8; i > 0; i--) {
+            Label rowLabel = new Label(String.valueOf(i));
+            rowLabel.setFont(new Font("Arial", 60));
+            vBox.getChildren().add(rowLabel);
         }
+
 
         hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setTranslateY(-150);
         for (int i = 1; i < 9; i++) {
-            Label colName = new Label(String.valueOf((char)(i + 96)));
-            hBox.getChildren().add(colName);
+            Label colLabel = new Label(String.valueOf((char)(i + 96)));
+            colLabel.setFont(new Font("Arial", 70));
+            hBox.getChildren().add(colLabel);
         }
 
-        message = new Label("");
+        message = new Label("ches s");
+        message.setFont(new Font("Arial", 80));
 
         borderPane = new BorderPane();
+        borderPane.setBottom(hBox);
+        borderPane.setLeft(vBox);
+        borderPane.setTop(message);
+        borderPane.setCenter(gridPane);
+        BorderPane.setAlignment(message, Pos.CENTER);
+
+        scene = new Scene(borderPane);
+        stage.setScene(scene);
+        stage.setTitle("ChessGUI");
+        stage.setResizable(true);
+        stage.setMaximized(true);
+        stage.show();
     }
 
     public void setButtonBackgroundImage(Button button, Square square) {
+        ChessPiece piece = square.getCurrentPiece();
+        BackgroundFill backgroundFill;
+        if (square.getSquareType() == Square.SquareType.DARK) {
+            backgroundFill = new BackgroundFill(Color.PERU, new CornerRadii(0), Insets.EMPTY);
+        } else {
+            backgroundFill = new BackgroundFill(Color.NAVAJOWHITE, new CornerRadii(0), Insets.EMPTY);
+        }
+        // The constructor for Background requires the BackgroundFill and BackgroundImage to be in
+        // a List or an array, which is why the singular fill and image is put in an array.
+        BackgroundFill[] fills = new BackgroundFill[1];
+        fills[0] = backgroundFill;
 
+        BackgroundImage backgroundImage = new BackgroundImage(
+                new Image(getClass().getResource("images/" + piece.toString() + ".png").toExternalForm()),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                new BackgroundSize(50, 50, true, true, true, false));
+        BackgroundImage[] images = new BackgroundImage[1];
+        images[0] = backgroundImage;
+
+        Background background = new Background(fills, images);
+        button.setBackground(background);
     }
 
     /**
@@ -92,8 +138,9 @@ public class ChessGUI extends Application {
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 Button button = new Button();
-                button.setMaxWidth(80);
-                button.setMaxHeight(80);
+                button.setMaxSize(80, 80);
+                button.setMinSize(10, 10);
+                button.setPrefSize(70, 70);
 
                 Square square = model.getBoard().getSquares()[row][col];
                 // Checks if the square has a piece on it. If occupied, it will update the
